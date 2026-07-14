1236c9b364c4b37cd1ad022772d8acd91d26306d
NazarShaikh <shaikhnazar265@gmail.com>
2026-07-14 13:40:20 +0530

    Fix helper registration and update MongoDB Atlas configuration

--- Back-end/controllers/authController.js
+++ Back-end/controllers/authController.js
@@ -10,14 +10,19 @@ export const register = async (req, res) => {
 
   const hashed = await bcrypt.hash(password, 10);
 
-  const user = await User.create({
-    name,
-    email,
-    password: hashed,
-    role,
-    experience:role === "helper" ? experience : "", 
-    skills: role === "helper" ? skills : [],
-  });
+const user = await User.create({
+  name,
+  email,
+  password: hashed,
+  role,
+  experience: role === "helper" ? experience || "" : "",
+  skills:
+    role === "helper"
+      ? Array.isArray(skills)
+        ? skills
+        : []
+      : [],
+});
 
   res.status(201).json({ message: "Registered" });
 };
